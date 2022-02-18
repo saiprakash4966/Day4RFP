fe936e6a0c410a85a71ae7c7ab1318b3e93060df
unknown <saiprakashbeemari@gmail.com>
2022-02-18 09:41:07 +0530

    Snakeladder

---
+++ SnakeLadder2.java
@@ -0,0 +1,19 @@
+package com.bl;
+
+import java.util.Scanner;
+
+public class SnakeLadder2
+{
+	
+		public static void main(String[] args)
+		{
+	        Scanner sc= new Scanner(System.in);
+	        System.out.println("Input the names of the one players:");
+	        String A=sc.next();      
+	        int playerAposition=0;
+	        int count=0;
+	        int x;
+	}
+
+
+}
